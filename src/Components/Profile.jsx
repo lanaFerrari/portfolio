e8579fec4cb6bb3d93d625edc1b818e97fc86cca
Lana Ferrari <ferrarirangel@gmail.com>
2022-01-20 23:56:09 +0000

    MVP

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,34 +1,71 @@
 export default function Profile() {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    var dataText = ["Call", "DM", "E-mail", "Message", "Shout"];
+
+    function typeWriter(text, i, fnCallback) {
+      if (i < text.length) {
+        document.querySelector(".call-me").innerHTML =
+          text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+        setTimeout(function () {
+          typeWriter(text, i + 1, fnCallback);
+        }, 300);
+      } else if (typeof fnCallback == "function") {
+        setTimeout(fnCallback, 700);
+      }
+    }
+
+    function StartTextAnimation(i) {
+      if (typeof dataText[i] == "undefined") {
+        setTimeout(function () {
+          StartTextAnimation(0);
+        }, 2000);
+      }
+
+      if (i < dataText[i].length) {
+        typeWriter(dataText[i], 0, function () {
+          StartTextAnimation(i + 1);
+        });
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+  });
+
   return (
     <section
       id="about"
       className="lateral-padding padding-right-left padding-top-bottom panel"
-      data-color="black"
+      data-color="green"
     >
-      <div className="padding-top-bottom">
-        <h2 className="padding-top-bottom">About</h2>
-        <div>
-          <div className=" border padding-top-bottom padding-right-left pink-bg">
-            {/* <img
-            src={require("../imgs/clouds.jpg")}
-            alt=""
-            className="bg-clouds"
-          /> */}
-            <div className="flex bg-clouds padding-top-bottom padding-right-left">
-              <div className="align-center">
-                <p className="frame-profile">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Pariatur aut aliquid repellat sekjaDNJS.
-                </p>
-              </div>
-              <div className="frame-profile">
-                <img
-                  src={require("../imgs/profile.png")}
-                  alt="Blong girl icon"
-                />
-              </div>
+      <h2 className="padding-top-bottom">About ------- ⫸</h2>
+
+      <div className="justify-center padding-top-m">
+        <div className="border grid-template padding-top-bottom padding-right-left">
+          <div className="invite-frame box-shadow">
+            <div className="frame-video">
+              <img src={require("../imgs/thankyou.gif")} alt="Thank you gif" />
             </div>
           </div>
+
+          <div className="profile-frame box-shadow">
+            <img src={require("../imgs/profile.png")} alt="Blong girl icon" />
+          </div>
+
+          <div className="about-frame box-shadow padding-top">
+            <div className="second-frame">
+              <p className="white-c m-font padding-right-left-two padding-top-bottom">
+                I code because I failed to be an astronaut.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="justify-content">
+        <div className="flex padding-top-bottom">
+          <img src="https://img.icons8.com/nolan/50/github.png" />
+          <img src="https://img.icons8.com/nolan/50/linkedin-circled.png" />
+          <img src="https://img.icons8.com/nolan/50/email-sign.png" />
         </div>
       </div>
     </section>
