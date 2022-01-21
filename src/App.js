e696ed211f68afc69e927862f0f7a4d335075869
Lana Ferrari <ferrarirangel@gmail.com>
2022-01-21 17:24:05 +0000

    Create expand menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
+import React from "react";
+import { useState } from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
+import Expand from "./Components/Expand";
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = useState(true);
   return (
     <>
-      <Header />
-      <Main />
+      <Header isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+      <Main isCollapsed={isCollapsed} />
       <Footer />
     </>
   );
---
+++ src/Components/Expand.jsx
@@ -0,0 +1,17 @@
+export default function Expand(props) {
+  return (
+    <section className="s-font padding-right-left flex-end ">
+      <a href="#about">
+        <div className="padding-menu">About()</div>
+      </a>
+
+      <a href="#works">
+        <div className="padding-menu">Works()</div>
+      </a>
+
+      <a href="#contact">
+        <div className="padding-menu">Contact()</div>
+      </a>
+    </section>
+  );
+}
